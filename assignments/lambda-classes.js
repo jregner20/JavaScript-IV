f0d2f49d6a0ac4f21eec8801e7d564bdcdbfc581
Jake Regner <jregner20@gmail.com>
2019-09-19 18:18:21 -0400

    Add 3 objects for each class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -54,3 +54,105 @@ class Person{
             return `${this.Student.name} has begun sprint challenge on ${subject}`
         }
     }
+
+///////////////   INSTRUCTORS  ///////////////////////////////////////////////
+
+    const roxas = new Instructor({
+        name: 'Roxas',
+        age: 'Eternal',
+        location: 'Realm',
+        specialty: 'Chemistry',
+        favLanguage: 'Filipino',
+        catchPhrase: 'Hey man!'
+    });
+
+    const puchner = new Instructor({
+        name: 'Puchner',
+        age: 50,
+        location: 'Room 3',
+        specialty: 'Anatomy',
+        favLanguage: 'Italian',
+        catchPhrase: 'Everybody go to class!'
+    });
+
+    const paloucek = new Instructor({
+        name: 'Ed',
+        age: 45,
+        location: 'Room 2',
+        specialty: 'History',
+        favLanguage: 'English',
+        catchPhrase: ''
+    });
+
+///////////   PROJECT MANAGERS     //////////////////////////////////////
+
+    const erickson = new PM({
+        name: 'Will',
+        age: 32,
+        location: 'Office',
+        specialty: 'Wisdom',
+        favLanguage: 'Latin',
+        catchPhrase: 'Oh great',
+        gradClassName: 'CS1',
+        favInstructor: 'Hauser',
+    });
+
+    const schmidt = new PM({
+        name: 'Don',
+        age: 80,
+        location: 'Chapel',
+        specialty: 'Religion',
+        favLanguage: 'German',
+        catchPhrase: '',
+        gradClassName: 'CS2',
+        favInstructor: 'Mitchell'
+    });
+    
+    const strubb = new PM({
+        name: 'Sam',
+        age: 30,
+        location: 'Nether',
+        specialty: 'Enforcer',
+        favLanguage: 'Evil',
+        catchPhrase: 'Yes, Dr. E',
+        gradClassName: 'CS3',
+        favInstructor: 'Dr E'
+    });
+
+    ///////////////////      STUDENTS   //////////////////////////////////////
+
+    const paul = new Student({
+        name: 'Paul',
+        age: 24,
+        location: 'Tosa',
+        specialty: 'Gambling',
+        favLanguage: 'English',
+        catchPhrase: 'Put that down big boy!',
+        previousBackground: 'Sales',
+        className: 'CS4',
+        favSubjects: ['Gambling', 'Basketball', 'Partying']
+    });
+
+    const simon = new Student({
+        name: 'Simon',
+        age: 24,
+        location: 'Mukwonago',
+        specialty: 'Music',
+        favLanguage: 'English',
+        catchPhrase: 'Ohhhhh!',
+        previousBackground: 'Backline Tech',
+        className: 'CS5',
+        favSubjects: ['Music', 'Basketball', 'Smoking']
+    });
+
+    const pat = new Student({
+        name: 'Pat',
+        age: 25,
+        location: 'Webster',
+        specialty: 'Working and ignoring friends',
+        favLanguage: 'English',
+        catchPhrase: 'Demita',
+        previousBackground: 'Construction',
+        className: 'CS6',
+        favSubjects: ['Construction', 'Ignoring his boys', 'Working']
+    });
