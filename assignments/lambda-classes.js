0dc6f65937278eec44dbab4b8ebc67442be1e73a
Jake Regner <jregner20@gmail.com>
2019-09-19 16:46:50 -0400

    Add Parent class to lambda-classes file

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,13 @@
 // CODE here for your Lambda Classes
+
+class Parent{
+    constructor(par){
+        this.name= par.name,
+        this.age= par.age,
+        this.location= par.location
+    }
+    speak(){
+        return `Hello my name is Fred, I am from Bedrock where ${this.name} and ${this.location} are the object's own props`;
+    }
+}
+
